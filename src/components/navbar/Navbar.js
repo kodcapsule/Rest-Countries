b3f9c1eedcaad2268c823ecc081d1d70d2a9d98e
Kuseh Simon Wewoliamo <kusehsw@gmail.com>
2023-02-11 09:36:53 +0000

    added media queries

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -4,17 +4,15 @@ import "./navbar.css";
 
 const Navbar = () => {
   return (
-    <header>
-      <nav className="container navbar">
-        <div className="navbar__container">
-          <h1>Where in the world?</h1>
-          <p>
-            <BsMoon />
-            <span>Dark mode</span>
-          </p>
-        </div>
-      </nav>
-    </header>
+    <nav className="container navbar">
+      <div className="navbar__container">
+        <h1>Where in the world?</h1>
+        <p>
+          <BsMoon />
+          <span>Dark mode</span>
+        </p>
+      </div>
+    </nav>
   );
 };
 
